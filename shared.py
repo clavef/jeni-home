65d8ba407e3de4e3ac87b48960eee068b0d4f0ad
clavef <id@matt.kr>
2025-04-01 15:13:51 +0900

    Update shared.py

--- shared.py
+++ shared.py
@@ -5,4 +5,4 @@ def show_menu(active_page: str):
     st.sidebar.title("🌟 제니앱")
     st.sidebar.page_link("home.py", label="홈", icon="🏠")
     st.sidebar.page_link("pages/audit.py", label="정산 도우미", icon="📊")
-    st.sidebar.page_link("pages/insta_unfollow_check.py", label="인스타 언팔체크", icon="📱")
+    st.sidebar.page_link("pages/check.py", label="인스타 언팔체크", icon="📱")
