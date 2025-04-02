e27043329670df4fcf274f3377914e7e6e5d91d5
clavef <id@matt.kr>
2025-04-02 22:42:31 +0900

    Update shared.py

--- shared.py
+++ shared.py
@@ -5,17 +5,18 @@ def show_menu(active_page: str):
     st.markdown("""
         <style>
             .sidebar-header {
-                text-align: center;
+                text-align: left;
                 margin-bottom: 1.5rem;
             }
             .sidebar-header img {
-                width: 100px;
-                margin-bottom: 0.3rem;
+                width: 140px;
+                margin-bottom: 0.5rem;
             }
             .sidebar-title {
-                font-size: 1.2rem;
-                font-weight: bold;
+                font-size: 1.15rem;
+                font-weight: 700;
                 color: #000000;
+                margin-bottom: 1rem;
             }
         </style>
     """, unsafe_allow_html=True)
@@ -24,7 +25,7 @@ def show_menu(active_page: str):
         """
         <div class="sidebar-header">
             <img src="https://raw.githubusercontent.com/clavef/jeniapp/main/logo.png">
-            <div class="sidebar-title">🌟 제니앱 (<a href='https://jeni.kr' target='_blank'>Jeni.kr</a>)</div>
+            <div class="sidebar-title">🌟 제니앱 (<a href='https://jeni.kr' target='_blank' style='color: #1f77b4; text-decoration: none;'>Jeni.kr</a>)</div>
         </div>
         """,
         unsafe_allow_html=True
