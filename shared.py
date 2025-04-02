218a63a35c79e231b7564d6f1e3b2bc3dbc2c882
clavef <id@matt.kr>
2025-04-03 00:04:27 +0900

    Update shared.py

--- shared.py
+++ shared.py
@@ -28,4 +28,4 @@ def show_menu(active_page: str):
 
     st.sidebar.page_link("pages/check.py", label="인스타 언팔체크", icon="📱")
     st.sidebar.page_link("pages/cards.py", label="카드값 계산기", icon="💳")
-    st.sidebar.page_link("pages/audit.py", label="정산 도우미", icon="📊")
+    st.sidebar.page_link("pages/_audit.py", label="정산 도우미", icon="📊")
